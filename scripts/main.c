86e1cf9109b56e4e5206a9a0b3bf8785950e47bb
collinshack <awusacollins@gmail.com>
2025-02-13 15:17:26 +0300

    inputs from user

--- scripts/main.c
+++ scripts/main.c
@@ -3,6 +3,18 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int number1;
+    printf("Enter first num: \n");
+    scanf("%d", &number1);
+    int number2;
+
+    printf("Enter second num: \n");
+
+    scanf("%d", &number2);
+    int number3;
+    printf("Enter third num: \n");
+
+    scanf("%d", &number3);
+    printf("Here is the total number you entered: %d", number1+number2/number3);
     return 0;
 }
